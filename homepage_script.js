4b4270e8e60e2a56de90b811619395176504c751
Mvnlst <martijn.vanleest@ziggo.nl>
2025-04-06 13:21:02 +0200

    transitions

--- homepage_script.js
+++ homepage_script.js
@@ -1,16 +1,65 @@
 let menu_items = document.getElementsByClassName("menu-item");
 let title = document.getElementsByClassName("title")[0];
 
+setTimeout(appear, 1000, 0);
+
+function appear(index){
+    if(index == menu_items.length) return;
+    menu_items[index].style.opacity = "1";
+    setTimeout(appear, 300, index+1);
+}
+
 
 function levels(){
-    window.location.href="levels.html";
+    disappear(-1, 0);
+    // window.location.href="levels.html";
 }
 
 function how_to_play(){
-    window.location.href="levels.html";
+    disappear(-1, 1);
+
+    // window.location.href="levels.html";
 }
 
 function what_is_rally(){
-    window.location.href="levels.html";
+    disappear(-1, 2);
+    // window.location.href="levels.html";
+}
+
+function disappear(index, skip){
+    if(index == menu_items.length){
+        menu_items[skip].style.transform = "scale(2)";
+        menu_items[skip].style.opacity = "0";
+        setTimeout(redirect, 800, skip);
+        return;
+    }
+    if(index == -1){
+        title.style.color = "var(--background-color)";
+        setTimeout(disappear, 500, index + 1, skip);
+    } else {
+        if(index != skip) {
+            menu_items[index].style.opacity = "0";
+            setTimeout(disappear, 300, index + 1, skip);
+
+        } else {
+            setTimeout(disappear, 0, index + 1, skip);
+        }
+    }
+}
+
+function redirect(index){
+    switch (index) {
+        case 0:
+            window.location.href="levels.html";
+            break;
+        case 1:
+            window.location.href="levels.html";
+            break;
+        case 2:
+            window.location.href="levels.html";
+            break;
+        default:
+            break;
+    }
 }
 
--- levels_script.js
+++ levels_script.js
@@ -1,93 +1,51 @@
-let grid = document.getElementsByClassName("grid")[0];
+let levels = document.getElementsByClassName("levels")[0];
 let wrapper = document.getElementsByClassName("wrapper")[0];
 let clear_button = document.getElementsByClassName("clear-mode")[0];
 let title = document.getElementsByClassName("title")[0];
-let navbar = document.getElementsByClassName("navbar")[0];
-
-
-let amountOfLevels = 4
-let width = 4;
-let height = 1;
+let level_tiles = [];
 let tileIndex = 0;
 
-grid.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
-grid.style.gridTemplateRows = `repeat(${height}, 1fr)`;
-grid.style.aspectRatio = `${width} / ${height}`;
+for(let index = 0; index < 4; index++){
+    let tile = document.createElement("div");
+    tile.classList.add("level-tile");
+    tile.innerHTML = `${index+1}`;
+    tile.onclick = () => removeTiles(index, 0);
+    levels.appendChild(tile);
+    level_tiles.push(tile);
+}
+setTimeout(start, 100)
 
-console.log(wrapper.clientHeight, wrapper.clientWidth)
+function start(){
+    title.style.opacity = "1";
+    setTimeout(grow, 500, 0);
+}
 
-check_screen()
-window.onresize=check_screen
-build();
-window.addEventListener('orientationchange', check_screen, true);
+function grow(index){
+    if(index == level_tiles.lenght) return;
+    level_tiles[index].style.transform = "scale(1)";
+    setTimeout(grow, 500, index + 1);
+}
 
 function homepage(){
     window.location.href = "index.html";
 }
 
-function check_screen() {
-    if(wrapper.clientHeight *width / height < wrapper.clientWidth) {
-        grid.classList.add("height");
-        grid.classList.remove("width");
+function removeTiles(index, current){
+    if(current == level_tiles.length){
+        title.style.opacity = "0";
+        level_tiles[index].style.opacity = "0";
+        level_tiles[index].style.cursor = "default";
+        setTimeout(redirect, 700, index + 1);
+    }
+    if(index != current) {
+        level_tiles[current].style.transform = "scale(0)";
+        setTimeout(removeTiles, 300, index, current+1);
     } else {
-        grid.classList.add("width");
-        grid.classList.remove("height");
+        setTimeout(removeTiles, 0, index, current+1);
     }
-    let max = Math.max(height, width);
-    let min = Math.min(height, width);
-    if(wrapper.clientHeight > wrapper.clientWidth) {
-       height = max;
-       width = min;
-       grid.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
-        grid.style.gridTemplateRows = `repeat(${height}, 1fr)`;
-        grid.style.aspectRatio = `${width} / ${height}`;
-    } else {
-        width = max;
-        height = min;
-        grid.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
-        grid.style.gridTemplateRows = `repeat(${height}, 1fr)`;
-        grid.style.aspectRatio = `${width} / ${height}`;
-    }
-    grid.style.fontSize = `${grid.clientWidth / (width * 2)}px`;
+    
 }
 
-function build() {
-    for(let amount = 0; amount < amountOfLevels; amount++){
-        let level_tile = document.createElement("div");
-        level_tile.classList.add("level-tile");
-        level_tile.onclick = () => {
-            window.location.href = `level${amount+1}.html`;
-        }
-        grid.appendChild(level_tile);
-    }
-}
-
-function create_empty_tile(){
-    let empty_tile = document.createElement("div");
-    empty_tile.classList.add("empty-tile");
-    grid.appendChild(empty_tile);
-    tileIndex++;
-}
-
-
-function grow(tile) {
-    tile.style.transform = "scale(1)";
-}
-
-function finish() {
-    for (let index = 0; index < edge_values.length; index++) {
-        if(edge_values[index] != visible[index]) return;
-    }
-    if(state.indexOf(0) == -1) {
-        close_animation();
-    }
-}
-
-function close_animation(){
-    for(let index = 0; index < tiles.length; index++){
-        setTimeout(shrink, Math.random()*2000, tiles[index]);
-    }
-}
-function shrink(tile) {
-    tile.style.transform = "scale(0)";
+function redirect(level){
+    location.href = `level${level}.html`;
 }
